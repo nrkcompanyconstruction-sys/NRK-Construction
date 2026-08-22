2f9fa80844c89a6ffb17e3331a556b1d07c21a8d
nrkcompanyconstruction-sys <nrkcompanyconstruction@gmail.com>
2026-08-22 17:39:30 +0530

    sitemap updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -39,18 +39,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/privacy-policy`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.3,
-    },
-    {
-      url: `${baseUrl}/terms-of-service`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.3,
-    },
+    }
   ];
 }
